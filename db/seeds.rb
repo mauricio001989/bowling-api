ecba096824704ecb6ea9250c61e1d50561f56932
maaoo <mauro001989@gmail.com>
2020-03-31 08:28:16 -0500

    migration, model and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Create user'
+user = User.create(name: 'mao', email: 'mao@ggg.com', document: '1111')
+
+puts 'Create Bowling lane'
+lane = BowlingLane.create(length: '19.20')
+
+puts 'Create Bowling games'
+BowlingGame.create(points: [{ point: 12 }, { point: 13 }], user: user, bowling_lane: lane)
